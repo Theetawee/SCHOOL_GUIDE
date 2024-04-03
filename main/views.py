59de63105aec5e38b64a6578b525781e2a379100
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-04-03 15:35:43 +0300

    save

--- main/views.py
+++ main/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
+    title = "StudyGuide - Your Premier E-Learning Platform for Online Tests, Past Papers, Books, and More"
+    description = "Explore StudyGuide, your ultimate destination for online tests, past papers, books, and a wealth of educational resources. Enhance your learning experience and excel in your studies with our comprehensive e-learning platform. Start your journey towards academic success today!"
+    context = {"title": title, "description": description}
+
     if "question" in request.GET:
         query = request.GET.get("question")
         results = Question.objects.filter(question_text__icontains=query)
-        if request.htmx:
+        if request.headers.get("htmx-request"):
             return render(request, "partials/results.html", {"results": results})
-    else:
-        results = Question.objects.none()
-    return render(request, "main/index.html", {"results": results})
+
+    return render(request, "main/index.html", context)
 
 
 def question_detail(request, pk):
@@ -30,4 +33,6 @@ def subjects(request):
 def questions_list(request, topic_slug):
     topic = Topic.objects.get(slug=topic_slug)
     questions = Question.objects.filter(topic=topic)
-    return render(request, "main/questions_list.html", {"questions": questions, "topic": topic})
+    return render(
+        request, "main/questions_list.html", {"questions": questions, "topic": topic}
+    )
